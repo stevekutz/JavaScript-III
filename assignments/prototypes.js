148dea3a14005c91097040104c24838523d190be
stevekutz <stkutz@gmail.com>
2019-01-22 22:32:32 -0600

    STRETCH - added Villian & Hero constructor

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -143,3 +143,12 @@ Humanoid.prototype.greet = function () { return `${this.name} offers a greeting 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+// Villian constructor
+function Villian(villianAttributes) {
+  Humanoid.call(this, villianAttributes);
+}
+
+function Hero(heroAttributes) {
+  Humanoid.call(this, heroAttributes);
+}
